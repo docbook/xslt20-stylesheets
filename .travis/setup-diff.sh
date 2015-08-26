8f95354f648b74762d3e67e39c02e0c3e29c4195
Norman Walsh <ndw@nwalsh.com>
2015-08-26 15:49:38 -0500

    Fix list

--- .travis/setup-diff.sh
+++ .travis/setup-diff.sh
@@ -7,7 +7,7 @@ then
     rm -rf delta
     mkdir delta
     cd delta
-    curl -s -o filelist -u "$DELTA_NAME:$DELTA_PASS" -s "$DELTA_BASE/filelist"
+    curl -s -o filelist -u "$DELTA_NAME:$DELTA_PASS" -s "$DELTA_BASE/smalllist"
     for f in `cat filelist`; do
         curl -s -o $f -u "$DELTA_NAME:$DELTA_PASS" -s "$DELTA_BASE/$f"
     done
@@ -17,7 +17,7 @@ then
     rm -rf delta
     mkdir delta
     cd delta
-    for f in `cat $DELTA_LOCAL/filelist`; do
+    for f in `cat $DELTA_LOCAL/smalllist`; do
         cp "$DELTA_LOCAL/$f" .
     done
 fi
