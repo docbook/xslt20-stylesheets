ac916ff27b5c8a81b0fecb7bf565a33d8937f4b9
Norman Walsh <ndw@nwalsh.com>
2018-07-21 10:54:24 -0400

    More cleanup

--- .travis/publish-release.sh
+++ .travis/publish-release.sh
@@ -24,22 +24,25 @@ SHA=$(git rev-parse --verify HEAD)
 # Clone the minimum of the CDN repo needed.
 CDN_REPO="https://$GH_TOKEN@github.com/docbook/cdn.git"
 git clone $CDN_REPO cdn --depth=1 -q
+
 # Clean out existing content...
 rm -rf cdn/release/xsl20/$VERSION
+
 # ...and copy the new one.
 mkdir -p cdn/release/xsl20/$VERSION
-rm -f cdn/release/xsl20/index.html
-cp -a build/distributions/docbook-xslt2-$VERSION.zip cdn/release/xsl20/$VERSION
-cd cdn/release/xsl20/$VERSION && unzip docbook-xslt2-$VERSION.zip && cd ../../../../
-
-ls -laR cdn/release/xsl20
+cd cdn/release/xsl20
+unzip -q build/distributions/docbook-xslt2-$VERSION.zip
+mv docbook-xslt2-$VERSION $VERSION
+cd ../../..
 
 # We could normally make "current" symbolic links to "snapshot"
 # but github's policy doesn't allow to publish symbolic links in pages.
+rm -rf cdn/release/xsl20/current
 mkdir -p cdn/release/xsl20/current
-rm -rf cdn/release/xsl20/current/*
 cp -a cdn/release/xsl20/$VERSION cdn/release/xsl20/current
 
+# Regenerate index files
+rm -f cdn/release/xsl20/index.html
 $here/generate_index.py cdn/release/xsl20
 
 ## Now prepare to commit and push to the CDN
